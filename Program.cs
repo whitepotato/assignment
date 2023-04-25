2a02019366cedb5db466e5f8633f41c0993521bb
whitepotato <imipu.stone@gmail.com>
2023-04-26 00:39:05 +0900

    new project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignetment
+{
+    internal class Car
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
